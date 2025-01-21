5f46db04f9f809ae7a295caa8351f3f48f2278cf
Ami0480 <amifuku80@gmail.com>
2025-01-21 17:02:19 +0800

    added the basic javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
+function showActivity(event) {
+  event.preventDefault();
 
+  let answerElement = document.querySelector("#answer");
+  answerElement.innerHTML = "hi";
+}
+
+let searchElement = document.querySelector("#submit-button");
+searchElement.addEventListener("click", showActivity);
