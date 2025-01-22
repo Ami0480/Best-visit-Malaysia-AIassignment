13533e6e136a608b3c1cbfc16c6bc363f7ae38d9
Ami0480 <amifuku80@gmail.com>
2025-01-22 13:58:04 +0800

    added AI API

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
+function displayActivity(response) {
+  new Typewriter("#answer", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function showActivity(event) {
   event.preventDefault();
 
-  new Typewriter("#answer", {
-    strings: "activity",
-    autoStart: true,
-    delay: 30,
-    cursor: "",
-  });
+  let searchFormInput = document.querySelector("#search-form-user");
+  let apiKey = "25ba4b8ct7fc123o0c3d6fccfc118bbd";
+  let prompt = `search about ${searchFormInput.value} in malaysia`;
+  let context =
+    "You are a knoledge of Malaysia. Provide 3 activities in Malaysia and write separate line with <br />.Start with Here are some popular activities you can enjoy in Malaysia!! with font-size:22px;. Use <div></div> each answer with <ul> and use <strong> element before ,.AmiCodes AI at the bottom with<strong> element";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayActivity);
 }
 
 let activityElement = document.querySelector("#search-form");
