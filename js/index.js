8464fa727fb89f7b00ca67e5137f9d077d033dc9
Ami0480 <amifuku80@gmail.com>
2025-01-22 14:47:29 +0800

    added User Experience

--- js/index.js
+++ js/index.js
@@ -14,8 +14,12 @@ function showActivity(event) {
   let apiKey = "25ba4b8ct7fc123o0c3d6fccfc118bbd";
   let prompt = `search about ${searchFormInput.value} in malaysia`;
   let context =
-    "You are a knoledge of Malaysia. Provide 3 activities in Malaysia and write separate line with <br />.Start with Here are some popular activities you can enjoy in Malaysia!! with font-size:22px;. Use <div></div> each answer with <ul>,use <strong> element before ,. AmiCodes AI at the bottom with<strong> element";
+    "You are a knoledge of Malaysia. Provide 3 activities in Malaysia and write separate line with <br />. Start with Here are some popular activities you can enjoy in Malaysia!! with font-size:22px;. Use <div></div> each answer with <ul>,use <strong> element before ,. AmiCodes AI at the bottom with<strong> element";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let displayAnswerElement = document.querySelector("#answer");
+  displayAnswerElement.classList.remove("hidden");
+  displayAnswerElement.innerHTML = `<div class="blink">⏳ Searching your activities about ${searchFormInput.value}</div>`;
 
   axios.get(apiURL).then(displayActivity);
 }
