7fa189b392d333cbb434c08667c4e80cc968aa5e
Ami0480 <amifuku80@gmail.com>
2025-01-22 14:59:15 +0800

    changed AI search to make specific answer

--- js/index.js
+++ js/index.js
@@ -13,8 +13,7 @@ function showActivity(event) {
   let searchFormInput = document.querySelector("#search-form-user");
   let apiKey = "25ba4b8ct7fc123o0c3d6fccfc118bbd";
   let prompt = `search about ${searchFormInput.value} in malaysia`;
-  let context =
-    "You are a knoledge of Malaysia. Provide 3 activities in Malaysia and write separate line with <br />. Start with Here are some popular activities you can enjoy in Malaysia!! with font-size:22px;. Use <div></div> each answer with <ul>,use <strong> element before ,. AmiCodes AI at the bottom with<strong> element";
+  let context = `You are a knoledge of Malaysia. Provide 3 specific about ${searchFormInput.value}things to do in Malaysia and write separate line with <br />. Start with Here are some popular activities you can enjoy in Malaysia!! with font-size:22px;. Use <div></div> each answer with <ul>,use <strong> element before ,. AmiCodes AI at the bottom with<strong> element`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let displayAnswerElement = document.querySelector("#answer");
